e7fccab3ed808bb5b0a972e466f3550364482a1b
Joey Wang <joey840404@gmail.com>
2023-04-07 11:40:12 -0700

    dynamic import react-force-graph

--- src/app/graph.tsx
+++ src/app/graph.tsx
@@ -1,14 +1,15 @@
 'use client'
 
+import dynamic from "next/dynamic";
 import React, {useRef, useEffect, useState} from 'react';
-import {ForceGraph2D} from 'react-force-graph';
 
 function Graph(){
-     let data = {
-      nodes: [{ id: "0x0000001", balance: 10, index: 0},
-              { id: "0x0000002", balance: 1, index: 1},
-              { id: "0x0000003", balance: 3, index: 2},
-              { id: "0x0000004", balance: 6, index: 3}
+    const ForceGraph2D = dynamic(() => import('react-force-graph').then((r) => r.ForceGraph2D), { ssr: false });
+    let data = {
+            nodes: [{ id: "0x0000001", balance: 10, index: 0},
+                    { id: "0x0000002", balance: 1, index: 1},
+                    { id: "0x0000003", balance: 3, index: 2},
+                    { id: "0x0000004", balance: 6, index: 3}
             ],
       links: [
         { source: "0x0000001", target: "0x0000002", value: 2},
