8a2b720948312f2669c8b2dcda8031553cbc1970
Joey Wang <joey840404@gmail.com>
2023-04-20 22:30:42 -0700

    graph with three buttons

--- src/app/graph.tsx
+++ src/app/graph.tsx
@@ -15,42 +15,122 @@ import ReactFlow, {
 } from 'reactflow';
 import 'reactflow/dist/style.css';
 
-function Node({ id, data }) {
+function Node({ id, data}) {
 
     const topHandleId = `${id}-t`;
-    const bottomHandleId = `${id}-b`;
+    const autoHandleId = `${id}-b-l`;
+    const moreHandleId = `${id}-b-m`;
+    const customHandleId = `${id}-b-r`;
 
-    const onMoreClicked = useCallback(() => {
-        data.addNode({addNode: data.addNode})
+    const onAutoClicked = useCallback(() => {
+        data.addNode({addNode: data.addNode,
+                      sourceId: id,
+                      xOffset: -650,
+                      yOffset: -100,
+                      handle: `l`})
     },[data]);
+    const onMoreClicked = useCallback(() => {
+        data.addNode({addNode: data.addNode,
+                      sourceId: id,
+                      xOffset: 0,
+                      yOffset: 20,
+                      handle: `m`})
+    },[data]);
+    const onCustomClicked = useCallback(() => {
+        data.addNode({addNode: data.addNode,
+                      sourceId: id,
+                      xOffset: 650,
+                      yOffset: -100,
+                      handle: `r`})
+    },[data]);
+
     const content =
-        `The "Unterminated string constant" error occurs when a string literal is not properly closed in your JavaScript or TypeScript code. When you paste multiline Markdown text directly into a string, it may cause this error because regular string literals in JavaScript don't support multiline content.`;
-        return (
-            <div>
-                <div className="h-fit-content border border-2 border-black bg-white text-black rounded-md max-w-xl">
-                    <h2 className="border-b-2 border-black p-4">Start Here </h2>
-                    <div className="border-b-2 border-black p-4 text-left">
-                        <ReactMarkdown>{content}</ReactMarkdown>
-                    </div>
-                    <div className="flex flex-row">
-                        <button className="basis-1/3 p-4 border-r-2 border-black hover:bg-gray-100">Auto</button>
-                        <button className="basis-1/3 p-4 border-r-2 border-black hover:bg-gray-100" onClick={onMoreClicked}>More</button>
-                        <button className="basis-1/3 p-4 hover:bg-gray-100">Custom</button>
-                    </div>
+        `
+Overview of Generative AI
+-------------------------
+
+Generative AI is a subfield of artificial intelligence that focuses on creating new data or content that resembles existing data. This technology has gained popularity due to its applications in art, music, text generation, and more. At the core of generative AI are generative models, which learn patterns and structures within data to create realistic outputs.
+
+### Top Level Learning Syllabus
+
+1. **Introduction to Generative AI**
+
+    *   Generative models vs. discriminative models
+    *   Types of generative models: parametric and non-parametric
+    *   Applications of generative AI
+2. **Probability and Statistics**
+
+    *   Probability distributions
+    *   Maximum likelihood estimation
+    *   Bayesian inference
+3. **Variational Autoencoders (VAEs)**
+
+    *   Encoder and decoder architectures
+    *   Variational inference
+    *   Applications of VAEs
+4. **Generative Adversarial Networks (GANs)**
+
+    *   Generator and discriminator networks
+    *   GAN training process
+    *   Variants of GANs (DCGAN, CycleGAN, etc.)
+    *   Applications of GANs
+5. **Autoregressive Models**
+
+    *   PixelRNN and PixelCNN
+    *   Sequence generation with RNNs and LSTMs
+    *   Applications of autoregressive models
+6. **Transformer-based Models**
+
+    *   Attention mechanism
+    *   GPT, GPT-2, and GPT-3
+    *   Applications of transformer-based models
+7. **Advanced Topics**
+
+    *   Conditional generation
+    *   Style transfer
+    *   Ethical considerations and challenges in generative AI
+        `
+    return (
+        <>
+            <div className="h-fit-content border border-2 border-black bg-white text-black rounded-md max-w-xl">
+                <h2 className="border-b-2 border-black p-4">Start Here </h2>
+                <div className="border-b-2 border-black p-4 text-left">
+                    <ReactMarkdown>{content}</ReactMarkdown>
                 </div>
-                <Handle
-                    type="sorce"
-                    position={Position.Top}
-                    id= {topHandleId}
-                />
-                <Handle
-                    type="sorce"
-                    position={Position.Bottom}
-                    id= {bottomHandleId}
-                />
+                <div className="flex flex-row">
+                    <button className="basis-1/3 p-4 border-r-2 border-black hover:bg-gray-100" onClick={onAutoClicked}>Auto</button>
+                    <button className="basis-1/3 p-4 border-r-2 border-black hover:bg-gray-100" onClick={onMoreClicked}>More</button>
+                    <button className="basis-1/3 p-4 hover:bg-gray-100" onClick={onCustomClicked}>Custom</button>
+                </div>
             </div>
-        )
+            <Handle
+                type = "target"
+                position = {Position.Top}
+                id = {topHandleId}
+            />
+            <Handle
+                type = "source"
+                position = {Position.Bottom}
+                style ={{left:60}}
+                id = {autoHandleId}
+            />
+            <Handle
+                type = "source"
+                position = {Position.Bottom}
+                id = {moreHandleId}
+            />
+            <Handle
+                type = "source"
+                position = {Position.Bottom}
+                style ={{right:60, left: 'auto'}}
+                id = {customHandleId}
+            />
+        </>
+    )
 }
+
+let id = 1
+const getId = () => `${id++}`
 
 function Flow(){
 
@@ -68,26 +148,32 @@ function Flow(){
     }, [reactFlowInstance]);
 
     const addNode = useCallback((props) => {
-        const nodeId = `node-${nodes.length - 1}`
-        const newNodeId = `node-${nodes.length}`;
-        const newEdgeId = `edge-${edges.length}`;
-        console.warn(nodes,nodeId, newNodeId, newEdgeId)
-        setNodes((nds) => nds.concat({id: newNodeId,
-                                      type: 'customNode',
-                                      position: {x: 0, y: 0},
-                                      data: {addNode: props.addNode}}));
-        //setEdges((eds) => eds.concat({newEdgeId,
-        //                              source: nodeId,
-        //                              target: newNodeId,
-        //                              targetHandle: `${newNodeId}-t`,
-         //                             sourceHandle:`${nodeId}-b`}));
-    }, [nodes])
 
-   const onInit = () => {
+        console.warn(nodes, props)
+        const id = getId()
+        const newNodeId = `node-${id}`;
+        const newEdgeId = `edge-${id - 1}`;
+        const sourceId = props.sourceId
+
+        setNodes((nds) => {
+            const sourceNode = nds.find(node => node.id === sourceId);
+            return nds.concat({id: newNodeId,
+                        type: 'customNode',
+                        position: {x: sourceNode.position.x + props.xOffset,
+                                   y: sourceNode.position.y + sourceNode.height + props.yOffset},
+                        data: {addNode: props.addNode}})});
+        setEdges((eds) => eds.concat({id: newEdgeId,
+                                      source: `${sourceId}`,
+                                      target: newNodeId,
+                                      targetHandle: `node-${id}-t`,
+                                      sourceHandle:`${sourceId}-b-${props.handle}`}));
+    },[nodes])
+
+    useEffect(() => {
        setNodes([{ id: "node-0" , type: 'customNode', position: {x: 0, y: 0}, data: {addNode: addNode}}])
-   };
+    },[]);
 
-    const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), [setEdges]);
+    //const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), [setEdges]);
 
     return <>
     <ReactFlow
@@ -96,12 +182,18 @@ function Flow(){
        edges={edges}
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
-        onConnect={onConnect}>
+        //onConnect={onConnect}
+        >
         <Controls className="bg-white" />
         <MiniMap />
         <Background variant="dots" gap={12} size={1} />
     </ReactFlow>
-            <button onClick={onInit}>init</button>
+            <button onClick={() => {console.log(nodes, edges)}}>init</button>
+            <button onClick={() => {setEdges([{id: "edge-0",
+                                               source: "node-0",
+                                               target:"node-1",
+                                               sourceHandle: "node-0-b",
+                                               targetHandle: "node-1-t"}])}}>edge</button>
     </>
 }
 
