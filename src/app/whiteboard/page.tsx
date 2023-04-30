9bd8b9551148569ffc261f4c079f8a79b5344232
Joey Wang <joey840404@gmail.com>
2023-04-30 12:23:12 -0700

    whiteboard new page

---
+++ src/app/whiteboard/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React, {useState} from 'react';
+
+export default function Home() {
+  return (
+    <main className="font-sans flex items-center justify-center w-screen h-screen flex-1 text-center">
+        <h1>White Board</h1>
+    </main>
+  )
+}
