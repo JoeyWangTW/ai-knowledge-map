e60b0154430ead6f04d5501ea74e8d64ef9ae7e2
Joey Wang <joey840404@gmail.com>
2023-04-30 12:20:27 -0700

    stream api edge function

---
+++ src/app/api/custom/route.ts
@@ -0,0 +1,21 @@
+import { OpenAIStream } from "../../utils/OpenAIStream";
+
+export const config = {
+  runtime: "edge",
+};
+
+export async function POST(req: Request) {
+    const { prompt } = (await req.json()) as {
+    prompt?: string;
+  };
+
+  const payload = {
+    model: "gpt-3.5-turbo",
+    messages: [{"role": "user", "content": prompt}],
+    stream: true,
+  };
+
+  const stream = await OpenAIStream(payload);
+  return new Response(stream);
+};
+
---
+++ src/app/utils/OpenAIStream.ts
@@ -0,0 +1,75 @@
+import { createParser, ParsedEvent, ReconnectInterval } from "eventsource-parser";
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export async function OpenAIStream(payload) {
+  const encoder = new TextEncoder();
+  const decoder = new TextDecoder();
+
+  let counter = 0;
+
+  /*
+    const openai = new OpenAIApi(configuration);
+
+    const topicOverviewResponse = await openai.createChatCompletion({
+        model: payload.model,
+      stream: payload.stream,
+        messages: [{role: "user", content: payload.prompt}],
+    });
+  */
+
+  console.log("Openai API", JSON.stringify(payload))
+  const res = await fetch("https://api.openai.com/v1/chat/completions", {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.OPENAI_API_KEY ?? ""}`,
+    },
+    method: "POST",
+    body: JSON.stringify(payload),
+  });
+
+  const stream = new ReadableStream({
+    async start(controller) {
+      function onParse(event: ParsedEvent | ReconnectInterval) {
+        if (event.type === "event") {
+          const data = event.data;
+          if (data === "[DONE]") {
+            controller.close();
+            return;
+          }
+          try {
+            const json = JSON.parse(data);
+            console.log(json.choices[0].delta.content)
+            const text = json.choices[0].delta.content;
+            if (text){
+
+            if (counter < 2 && (text.match(/\n/) || []).length) {
+              return;
+            }
+            const queue = encoder.encode(text);
+            controller.enqueue(queue);
+            counter++;
+            }
+          } catch (e) {
+            controller.error(e);
+          }
+        }
+      }
+
+     // stream response (SSE) from OpenAI may be fragmented into multiple chunks
+     // this ensures we properly read chunks & invoke an event for each SSE event stream
+     const parser = createParser(onParse);
+
+      // https://web.dev/streams/#asynchronous-iteration
+      for await (const chunk of res.body as any) {
+        parser.feed(decoder.decode(chunk));
+      }
+    },
+  });
+
+  return stream;
+}
