eda52ebf24dba7edc1198eb497fdd6d157fe2106
Joey Wang <joey840404@gmail.com>
2023-04-30 22:25:01 -0700

    remove streaming prototype

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,58 +5,6 @@ import { useRouter } from "next/navigation";
 import { Analytics } from "@vercel/analytics/react";
 import va from "@vercel/analytics";
 
-const OpenAIStream: React.FC = () => {
-  const [generatedJoke, setGeneratedJoke] = useState<String>("");
-  const [loading, setLoading] = useState("true");
-  const prompt = "tell me a story";
-
-  const generateJoke = async (e: any) => {
-    setGeneratedJoke("");
-    setLoading(true);
-    const response = await fetch("/api/custom", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        prompt,
-      }),
-    });
-
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
-
-    const data = response.body;
-    if (!data) {
-      return;
-    }
-    const reader = data.getReader();
-    const decoder = new TextDecoder();
-    let done = false;
-
-    while (!done) {
-      const { value, done: doneReading } = await reader.read();
-      done = doneReading;
-      console.warn(value);
-      const chunkValue = decoder.decode(value);
-      setGeneratedJoke((prev) => prev + chunkValue);
-    }
-
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    generateJoke();
-  }, []);
-
-  return (
-    <div>
-      <p className="text-white">{generatedJoke}</p>
-    </div>
-  );
-};
-
 export default function Home() {
   const router = useRouter();
   const [topic, setTopic] = useState("");
@@ -88,7 +36,6 @@ export default function Home() {
           </div>
           <button
             type="submit"
-            onClick={handleTopicSubmit}
             className="w-96 rounded-xl border border-white p-4 hover:border-b-2 hover:border-r-2"
           >
             Create Knowledge Map
