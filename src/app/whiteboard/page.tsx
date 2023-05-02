ea6ddab8cdb29ce6c84739b46591d34ea75a4d4c
Joey Wang <joey840404@gmail.com>
2023-05-02 00:05:04 -0700

    import export buttons

--- src/app/whiteboard/page.tsx
+++ src/app/whiteboard/page.tsx
@@ -3,7 +3,7 @@
 import React, { useState } from "react";
 import Whiteboard from "./whiteboard";
 import useStore, { RFState } from "../whiteboard/store";
-import { XMarkIcon, DocumentPlusIcon } from "@heroicons/react/24/outline";
+import { XMarkIcon, DocumentPlusIcon, ArrowDownOnSquareIcon, ArrowLeftOnRectangleIcon } from "@heroicons/react/24/outline";
 import { shallow } from "zustand/shallow";
 
 const selector = (state: RFState) => ({
@@ -68,25 +68,72 @@ function PromptModal() {
 
 function NewNodeButton() {
   const { onSetShowModal } = useStore(selector, shallow);
-
   return (
     <button
-      className="absolute top-5 left-5 h-10 w-10 text-zinc-800 bg-gray-100 hover:bg-gray-200 rounded-full p-2"
+      className="group relative h-10 w-max text-zinc-800 bg-gray-100 hover:bg-gray-200 rounded-full
+                 focus:outline-none transition-all duration-200 ease-out"
       onClick={() => {
         onSetShowModal(true);
       }}
     >
-      <DocumentPlusIcon />
+      <div className="flex items-center ">
+        <DocumentPlusIcon className="h-10 w-10 p-2"/>
+        <span className="hidden group-hover:inline-block transform transition-all duration-200 ease-out pr-2">
+          Add New Block
+        </span>
+      </div>
     </button>
   );
 }
+
+function ImportButton() {
+  return (
+    <button
+      className="group relative h-10 w-max text-zinc-800 bg-gray-100 hover:bg-gray-200 rounded-full
+                 focus:outline-none transition-all duration-200 ease-out"
+      onClick={() => {
+        alert("export");
+      }}
+    >
+      <div className="flex items-center ">
+        <ArrowLeftOnRectangleIcon className="h-10 w-10 p-2"/>
+        <span className="hidden group-hover:inline-block transform transition-all duration-200 ease-out pr-2">
+          Import
+        </span>
+      </div>
+    </button>
+  );
+}
+
+function ExportButton() {
+  return (
+    <button
+      className="group relative h-10 w-max text-zinc-800 bg-gray-100 hover:bg-gray-200 rounded-full focus:outline-none transition-all duration-200 ease-out"
+      onClick={() => {
+        alert("export");
+      }}
+    >
+      <div className="flex items-center ">
+        <ArrowDownOnSquareIcon className="h-10 w-10 p-2"/>
+        <span className="hidden group-hover:inline-block transform transition-all duration-200 ease-out pr-2">
+          Export
+        </span>
+      </div>
+    </button>
+  );
+}
+
 
 export default function Home() {
   return (
     <main className="font-sans flex items-center justify-center w-screen h-screen flex-1 text-center relative">
       <Whiteboard />
       <PromptModal />
-      <NewNodeButton />
+      <div className="absolute top-5 left-5 flex flex-col space-y-4">
+        <ImportButton/>
+        <ExportButton/>
+        <NewNodeButton />
+      </div>
     </main>
   );
 }
