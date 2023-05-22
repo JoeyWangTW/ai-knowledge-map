69c33698550743f5fc40906db4791d353bee4ad5
Joey Wang <joey840404@gmail.com>
2023-05-21 20:18:44 -0700

    remove analytics on graph page

--- src/app/graph/page.tsx
+++ src/app/graph/page.tsx
@@ -2,12 +2,10 @@
 
 import React, {useState} from 'react';
 import Graph from "./graph"
-import { Analytics } from '@vercel/analytics/react';
 
 export default function Home() {
   return (
     <main className="font-sans flex items-center justify-center w-screen h-screen flex-1 text-center">
-        <Analytics/>
         <Graph />
     </main>
   )
